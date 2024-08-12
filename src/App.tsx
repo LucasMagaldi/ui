3cd26eb5588bdc0ef149fa6a31878819d7809958
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-08-11 23:42:29 -0300

    feat: track secret words

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,31 @@ import './index.css'
 
 
 function App() {
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const matrixLines = document.querySelectorAll<HTMLDivElement>('.matrix-line');
-      matrixLines.forEach((line) => {
-        const text = String.fromCharCode(0x30A0 + Math.random() * 96);
-        line.innerText = text;
-      });
-    }, 50);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return (
-    <div className="matrix-bg">
-      {Array.from({ length: Math.floor(window.innerWidth / 20) }).map((_, columnIndex) => (
-        <div key={columnIndex} className="matrix-column" style={{ left: `${columnIndex * 20}px` }}>
-          {Array.from({ length: 20 }).map((_, lineIndex) => (
-            <div key={lineIndex} className="matrix-line">|</div>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  const words = ['APP_REGISTRATION', '@MSAL', 'ID', 'TENANT', 'Oauth 2.0', 'OIDC'];
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        const matrixLines = document.querySelectorAll<HTMLDivElement>('.matrix-line');
+        matrixLines.forEach((line) => {
+          const randomIndex = Math.floor(Math.random() * words.length);
+          line.innerText = Math.random() < 0.05 ? words[randomIndex] : String.fromCharCode(0x30A0 + Math.random() * 96);
+        });
+      }, 50);
+  
+      return () => clearInterval(intervalId); // Cleanup interval on unmount
+    }, []);
+  
+    return (
+      <div className="matrix-bg">
+        {Array.from({ length: Math.floor(window.innerWidth / 20) }).map((_, columnIndex) => (
+          <div key={columnIndex} className="matrix-column" style={{ left: `${columnIndex * 20}px` }}>
+            {Array.from({ length: 20 }).map((_, lineIndex) => (
+              <div key={lineIndex} className="matrix-line">|</div>
+            ))}
+          </div>
+        ))}
+        <div>Look again</div>
+      </div>
+    );
 }
 
 export default App
